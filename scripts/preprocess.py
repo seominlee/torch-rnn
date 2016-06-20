4b664ca9bf7fb53d069530498d8990dd7c6083e6
Vitaly Ostrosablin <tmp6154@yandex.ru>
2016-06-20 11:13:46 +0000

    Modified dreavjr's preprocess.py for syllabic prediction based on Pyphen library

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -4,6 +4,7 @@ import argparse, json, os
 import numpy as np
 import h5py
 import codecs
+import sys
 
 
 parser = argparse.ArgumentParser()
@@ -13,6 +14,7 @@ parser.add_argument('--output_json', default='data/tiny-shakespeare.json')
 parser.add_argument('--val_frac', type=float, default=0.1)
 parser.add_argument('--test_frac', type=float, default=0.1)
 parser.add_argument('--quiet', action='store_true')
+parser.add_argument('--syllabic', default='none')
 parser.add_argument('--encoding', default='utf-8')
 args = parser.parse_args()
 
@@ -21,14 +23,95 @@ if __name__ == '__main__':
   if args.encoding == 'bytes': args.encoding = None
 
   # First go the file once to see how big it is and to build the vocab
-  token_to_idx = {}
-  total_size = 0
-  with codecs.open(args.input_txt, 'r', args.encoding) as f:
-    for line in f:
-      total_size += len(line)
-      for char in line:
-        if char not in token_to_idx:
-          token_to_idx[char] = len(token_to_idx) + 1
+  if args.syllabic == 'none' :
+    syllabic = False
+    token_to_idx = {}
+    total_size = 0
+    with codecs.open(args.input_txt, 'r', args.encoding) as f:
+      for line in f:
+        total_size += len(line)
+        for char in line:
+          if char not in token_to_idx:
+            token_to_idx[char] = len(token_to_idx) + 1
+  else : 
+      syllabic = True
+
+      import unicodedata
+      import pyphen
+      if not (args.syllabic in pyphen.LANGUAGES) :
+	fallback = pyphen.language_fallback(args.syllabic)
+	if fallback is None:
+          print 'Syllabic dictionary', args.syllabic, 'is unavailable'
+          sys.exit(0)
+        else:
+	  print 'Warning: dictionary', args.syllabic, 'is unavailable, using', fallback, 'as fallback'
+	  args.syllabic = fallback
+      primary = pyphen.Pyphen(lang=args.syllabic)
+      if args.syllabic == 'en_US':
+	secondary = None
+      else:
+	secondary = pyphen.Pyphen(lang='en_US')
+
+      def scanSyllables(stream, encoding, processing) : 
+        word = ''
+        space = False
+        with codecs.open(stream, 'r', encoding) as f:
+          for line in f:
+            for char in line:
+              cat = unicodedata.category(char)
+              if cat[0]=='L' :
+                  word = word + char
+                  space = False
+                  continue
+              if len(word)>0 :
+		  splits = primary.positions(word.lower())
+		  if secondary:
+		    secsplits = secondary.positions(word.lower())
+		  else:
+		    secsplits = []
+		  if len(secsplits) > len(splits):
+		    splits = secsplits
+		  syls = splitWord(word.lower(), splits)
+                  word = ''
+              else :
+                  syls = [ ]
+              if cat[0]=='Z' :
+                if not space : syls.append( u' ' )
+                space = True              
+              elif cat[0]=='N' or cat[0]=='P' :
+                syls.append( char )
+                space = False
+              elif char == u'\n' :
+                syls.append( char )
+                space = False
+              for syl in syls :
+                  processing(syl)
+
+      def createVocab(syl) : 
+        global token_to_idx
+        global total_size
+        total_size += 1
+        if syl not in token_to_idx:
+          token_to_idx[syl] = len(token_to_idx) + 1
+      
+      def splitWord(word, splits) :
+	if len(splits) == 0:
+	  return [word]
+	else:
+	  firstidx = 0
+	  lastidx = 0
+	  syls = []
+	  while splits:
+	    firstidx = lastidx
+	    lastidx = splits.pop(0)
+	    syls.append(word[firstidx:lastidx])
+	  syls.append(word[lastidx:])
+	  return syls
+	    
+
+      token_to_idx = { u'\n' : 1 }
+      total_size = 0
+      scanSyllables(args.input_txt, args.encoding, createVocab)
 
   # Now we can figure out the split sizes
   val_size = int(args.val_frac * total_size)
@@ -58,14 +141,39 @@ if __name__ == '__main__':
 
   # Go through the file again and write data to numpy arrays
   split_idx, cur_idx = 0, 0
-  with codecs.open(args.input_txt, 'r', args.encoding) as f:
-    for line in f:
-      for char in line:
-        splits[split_idx][cur_idx] = token_to_idx[char]
-        cur_idx += 1
-        if cur_idx == splits[split_idx].size:
-          split_idx += 1
-          cur_idx = 0
+  if not syllabic : 
+    with codecs.open(args.input_txt, 'r', args.encoding) as f:
+      for line in f:
+        for char in line:
+          splits[split_idx][cur_idx] = token_to_idx[char]
+          cur_idx += 1
+          if cur_idx == splits[split_idx].size:
+            split_idx += 1
+            cur_idx = 0
+  else :
+
+    def convertInput(syl) : 
+      global check_size
+      global splits
+      global split_idx
+      global cur_idx
+      global token_to_idx
+      check_size += 1
+      # print check_size, syl
+      splits[split_idx][cur_idx] = token_to_idx[syl]
+      cur_idx += 1
+      if cur_idx == splits[split_idx].size:
+        split_idx += 1
+        cur_idx = 0
+
+    check_size = 0
+    scanSyllables(args.input_txt, args.encoding, convertInput)
+
+    if total_size != check_size :
+      print 'WARNING : File sizes mismatched between vocabulary building (', total_size, ') and token conversion (', check_size, ')'
+    if cur_idx!=0  :
+      print 'ERROR : File size mismatched between splits. cur_idx =', cur_idx
+      sys.exit(1)
 
   # Write data to HDF5 file
   with h5py.File(args.output_h5, 'w') as f:
